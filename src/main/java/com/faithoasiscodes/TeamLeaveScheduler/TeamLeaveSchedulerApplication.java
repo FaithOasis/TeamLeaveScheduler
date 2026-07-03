5dd82e62cb26f78808fc519ff43a730ff7837602
FaithOasis <mushangwefaithoasis@gmail.com>
2026-07-03 16:02:17 +0200

    Initial commit

---
+++ src/main/java/com/faithoasiscodes/TeamLeaveScheduler/TeamLeaveSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.faithoasiscodes.TeamLeaveScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamLeaveSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamLeaveSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faithoasiscodes/TeamLeaveScheduler/TeamLeaveSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faithoasiscodes.TeamLeaveScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamLeaveSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
